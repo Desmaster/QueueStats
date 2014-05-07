7476c35e1230c37a814144532558129fc61d4021
Timon de Groot <tdegroot96@gmail.com>
2014-05-07 11:00:30 +0200

    Adding normal stats!

--- src/views/SummonerView.xaml.cs
+++ src/views/SummonerView.xaml.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using RiotSharp;
 using src;
 
@@ -11,7 +12,8 @@ namespace src.views {
     public partial class SummonerView : Window, SummonerListener {
 
         private Summoner summoner, previous;
-        private List<LeagueItem> leagues; 
+        private List<LeagueItem> leagues;
+        private List<PlayerStatsSummary> summaries; 
         private Region region;
         private String summonerName;
         private RiotApi riotApi;
@@ -34,11 +36,17 @@ namespace src.views {
 
         private async Task loadSummoner() {
             leagues = await summoner.GetLeaguesAsync();
+            summaries = await summoner.GetStatsSummariesAsync();
         }
 
         private void init(Task task) {
             if(summoner == null || summoner.Id == 0) return;
             resetControls();
+            initRanked();
+            initNormal();
+        }
+
+        private void initRanked() {
             if (leagues == null) return;
             lblSummonerName.Content = summoner.Name;
             lblLevel.Content = "level " + summoner.Level;
@@ -70,46 +78,59 @@ namespace src.views {
                 }
                 MiniSeries series = league.MiniSeries;
                 if (series != null) {
-                    if (!(series.Losses == 0 && series.Wins == 0)) {
-                        lblSeriesTag.Visibility = Visibility.Visible;
-                        grdSeries.Visibility = Visibility.Visible;
-                        grdSeries.Children.Clear();
+                if (!(series.Losses == 0 && series.Wins == 0)) {
+                    lblSeriesTag.Visibility = Visibility.Visible;
+                    grdSeries.Visibility = Visibility.Visible;
+                    grdSeries.Children.Clear();
                         for (int j = 0; j < series.Progress.Length; j++) {
-                            char c = series.Progress[j];
-                            Log.info("" + c);
-                            switch (c) {
-                                case 'W':
-                                    Image img1 = new Image();
-                                    img1.Source = Util.CreateImage(Core.getInstance().getCurrentPath() + @"\img\profile\series_win.png");
-                                    Grid.SetColumn(img1, j);
-                                    Grid.SetRow(img1, 0);
-                                    grdSeries.Children.Add(img1);
-                                    break;
-                                case 'L':
-                                    Image img2 = new Image(); 
-                                    img2.Source = Util.CreateImage(Core.getInstance().getCurrentPath() + @"\img\profile\series_lose.png");
-                                    Grid.SetColumn(img2, j);
-                                    Grid.SetRow(img2, 0);
-                                    grdSeries.Children.Add(img2);
-                                    break;
-                                case 'N':
-                                    Image img3 = new Image();
-                                    img3.Source = Util.CreateImage(Core.getInstance().getCurrentPath() + @"\img\profile\series_none.png");
-                                    Grid.SetColumn(img3, j);
-                                    Grid.SetRow(img3, 0);
-                                    grdSeries.Children.Add(img3);
-                                    break;
-                            }
+                        char c = series.Progress[j];
+                        Log.info("" + c);
+                        switch (c) {
+                            case 'W':
+                            Image img1 = new Image();
+                            img1.Source = Util.CreateImage(Core.getInstance().getCurrentPath() + @"\img\profile\series_win.png");
+                            Grid.SetColumn(img1, j);
+                            Grid.SetRow(img1, 0);
+                            grdSeries.Children.Add(img1);
+                            break;
+                            case 'L':
+                            Image img2 = new Image();
+                            img2.Source = Util.CreateImage(Core.getInstance().getCurrentPath() + @"\img\profile\series_lose.png");
+                            Grid.SetColumn(img2, j);
+                            Grid.SetRow(img2, 0);
+                            grdSeries.Children.Add(img2);
+                            break;
+                            case 'N':
+                            Image img3 = new Image();
+                            img3.Source = Util.CreateImage(Core.getInstance().getCurrentPath() + @"\img\profile\series_none.png");
+                            Grid.SetColumn(img3, j);
+                            Grid.SetRow(img3, 0);
+                            grdSeries.Children.Add(img3);
+                            break;
                         }
-                    } else {
-                        lblSeriesTag.Visibility = Visibility.Hidden;
-                        grdSeries.Visibility = Visibility.Hidden;
                     }
                 } else {
                     lblSeriesTag.Visibility = Visibility.Hidden;
                     grdSeries.Visibility = Visibility.Hidden;
                 }
+                } else {
+                    lblSeriesTag.Visibility = Visibility.Hidden;
+                    grdSeries.Visibility = Visibility.Hidden;
+                }
             }
+        }
+
+        private void initNormal() {
+            if (summaries == null) return;
+            lblAramWins.Content = summaries[0].Wins;
+            lblAramTakedowns.Content = summaries[0].AggregatedStats.TotalChampionKills;
+            lblAramTurrets.Content = summaries[0].AggregatedStats.TotalTurretsKilled;
+            lblDominionWins.Content = summaries[3].Wins;
+            lblDominionNodes.Content = summaries[3].AggregatedStats.TotalNodeCapture;
+            lblDominionKills.Content = summaries[3].AggregatedStats.TotalChampionKills;
+            lbl3v3Wins.Content = summaries[9].Wins;
+            lbl3v3Kills.Content = summaries[9].AggregatedStats.TotalChampionKills;
+            lbl3v3Turrets.Content = summaries[9].AggregatedStats.TotalTurretsKilled;
         }
 
         private void resetControls() {
@@ -126,28 +147,30 @@ namespace src.views {
 
         private void hideControls() {
             resetControls();
-            lblSummonerName.Visibility = Visibility.Hidden;
-            lblLevel.Visibility = Visibility.Hidden;
-            lblTitleRanked.Visibility = Visibility.Hidden;
-            lblTitleLeagueName.Visibility = Visibility.Hidden;
-            lblTitleLeaguePoints.Visibility = Visibility.Hidden;
-            lblTitleWins.Visibility = Visibility.Hidden;
-            lblTitleDivision.Visibility = Visibility.Hidden;
-            lblTitleMedals.Visibility = Visibility.Hidden;
-            lblWins.Visibility = Visibility.Hidden;
+//            lblSummonerName.Visibility = Visibility.Hidden;
+//            lblLevel.Visibility = Visibility.Hidden;
+//            lblTitleRanked.Visibility = Visibility.Hidden;
+//            lblTitleLeagueName.Visibility = Visibility.Hidden;
+//            lblTitleLeaguePoints.Visibility = Visibility.Hidden;
+//            lblTitleWins.Visibility = Visibility.Hidden;
+//            lblTitleDivision.Visibility = Visibility.Hidden;
+//            lblTitleMedals.Visibility = Visibility.Hidden;
+//            lblWins.Visibility = Visibility.Hidden;
+            grdRanked.Visibility = Visibility.Hidden;
         }
 
         private void showControls() {
-            lblWins.Visibility = Visibility.Visible;
-            lblSummonerName.Visibility = Visibility.Visible;
-            lblLevel.Visibility = Visibility.Visible;
-            lblTitleRanked.Visibility = Visibility.Visible;
-            lblTitleLeagueName.Visibility = Visibility.Visible;
-            lblTitleLeaguePoints.Visibility = Visibility.Visible;
-            lblTitleWins.Visibility = Visibility.Visible;
-            lblTitleDivision.Visibility = Visibility.Visible;
-            lblTitleMedals.Visibility = Visibility.Visible;
-            lblWins.Visibility = Visibility.Visible;
+//            lblWins.Visibility = Visibility.Visible;
+//            lblSummonerName.Visibility = Visibility.Visible;
+//            lblLevel.Visibility = Visibility.Visible;
+//            lblTitleRanked.Visibility = Visibility.Visible;
+//            lblTitleLeagueName.Visibility = Visibility.Visible;
+//            lblTitleLeaguePoints.Visibility = Visibility.Visible;
+//            lblTitleWins.Visibility = Visibility.Visible;
+//            lblTitleDivision.Visibility = Visibility.Visible;
+//            lblTitleMedals.Visibility = Visibility.Visible;
+//            lblWins.Visibility = Visibility.Visible;
+            grdRanked.Visibility = Visibility.Visible;
         }
 
         public void summonerUpdated(Summoner summoner) {
